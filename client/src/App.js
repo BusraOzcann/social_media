47d52578ad52590b3f3f9d77f5c5e45365297534
Büşra Özcan <busraozcan@Busra-MacBook-Pro.local>
2023-03-16 22:52:41 +0300

    backend eklendi

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import Home from './pages/home/Home'
+import Profile from './pages/profile/Profile'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from "react-router-dom"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/profile/:username" element={<Profile/>}/>
+    </Route>
+  )
+)
+
+function App() {
+  return (
+    <RouterProvider router={router}/>
+  );
+}
+
+export default App;
